bffe892357db3a421b2f78237db3230d696d1fa6
水村友哉 <s21820106@nucba.ac.jp>
2024-09-21 20:11:47 +0900

    helloはテストの為削除 item/createで新規作成のテスト 成功

---
+++ app/api/item/create/route.js
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+
+    console.log(await req.json());
+
+    return NextResponse.json(
+        { 
+            message: 'アイテム'
+        }
+    );
+}
